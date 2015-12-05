d669da4b24c32447ae2c5d2c359c7d323d176e4e
John Hutchinson <john.hutchinson@pcs-instruments.com>
2015-12-05 10:22:39 +0000

    Initial commit

---
+++ Bowling Game/Bowling Game/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Bowling_Game
+{
+   public class Game {
+      public int Score { get; internal set; }
+   }
+
+   [TestFixture]
+   public class GameTests {
+
+      [Test]
+      public void TestNoScore() {
+         Game g = new Game();
+         Assert.That(g.Score, Is.EqualTo(0));
+      }
+   }
+}
